f521f6625f297b070463a21c9d165d06c6ed13dd
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-03-25 13:36:48 +0100

    added test for arry of all orders

--- src/tests/ordersAPI.test.ts
+++ src/tests/ordersAPI.test.ts
@@ -1,9 +1,7 @@
-import { beforeAll, afterEach, afterAll, describe, it, expect } from 'vitest'
+import {  describe, it, expect } from 'vitest'
 import { orderData } from '../types/ productsOders'
-/* import { server } from '../mocks/server'
 import * as ordersAPI from '../services/ordersAPI'
-import { orderData } from '../types/ productsOders'
- */
+// import { server } from '../mocks/server'
 
 /* // 🏎️ Boot API mocking
 beforeAll(() => {
@@ -34,6 +32,12 @@ const newOrder: orderData = {
 describe('ordersAPI', () => {
 	// kan hämta alla ordrar
 	it("should return a list of all the orders", async () =>{
+		const ordersResponse = await ordersAPI.getOrders()
+		expect(ordersResponse.status).toBe("success")
+		expect( Array.isArray(ordersResponse.data) ).toBe(true)
+
+		console.log('get products', ordersResponse.status)
+		console.log('get products', ordersResponse.data)
 
 	})
 	//kan skapa en ny order
@@ -45,3 +49,59 @@ describe('ordersAPI', () => {
 
 	})
 })
+
+
+export { ordersAPI }
+/*
+describe('ProductAPI', () => {
+	//kan hämta alla produkter
+	it('should return a list of all the products', async () => {
+		const productsResponse = await productAPI.getResponseAllproducts()
+		expect(productsResponse.status).toBe("success")
+		expect( Array.isArray(productsResponse.data) ).toBe(true)
+
+		console.log('get products', productsResponse.status)
+		console.log('get products', productsResponse.data)
+
+	})
+
+	//kan skapa en ny produkt
+	it('Should create a product', async () =>{
+		const product = await productAPI.createProduct(newProduct)
+		console.log("this is the product", product)
+
+		expect(product.data).toMatchObject({
+			id: expect.any(Number),
+			name: "Pelles Pangare",
+			description: "so goooooood",
+			price:  3,
+			images: {
+						thumbnail: "/storage/products/thumbnails/1997509-300x300.png",
+						large: "/storage/products/1997509.png"
+					},
+			stock_status: "instock",
+			stock_quantity: 4,
+
+			//order_items: []
+		})
+
+	})
+	//kan hämta den skapade produkten
+	it('Should create and then get the product', async () => {
+		const createdproduct = await productAPI.createProduct(newProduct)
+		const createdproductData = await productAPI.getProduct(createdproduct.data.id)
+		expect(createdproductData).toStrictEqual(createdproduct)
+
+	})
+
+	it('should create and then find the product among all products', async () => {
+			// create a new product
+			const product = await productAPI.createProduct(newProduct)
+			// get all products
+			const productsResponse = await productAPI.getResponseAllproducts()
+		expect(productsResponse.status).toBe("success")
+			//expect created product to exist in the array pf all products
+		expect(productsResponse.data).toContainEqual(product.data)
+	})
+})
+ */
